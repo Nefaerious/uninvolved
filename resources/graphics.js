edb41a2e08f9fda1d2dbc59d6b408332cc5453b0
nefaerious <samshin102@gmail.com>
2017-11-11 19:47:04 -0600

    testing for townhouse

--- resources/graphics.js
+++ resources/graphics.js
@@ -1,6 +1,6 @@
 var game = new PIXI.Container();
 var renderer = PIXI.autoDetectRenderer(256,256);
-var sprite, state;
+var townhouse, state;
 
 renderer.backgroundColor = 0xf0f0f0;
 renderer.view.style.position = "absolute";
@@ -10,27 +10,22 @@ renderer.resize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.view);
 
-var rectangle = new PIXI.Graphics();
-rectangle.beginFill(0x66CCFF);
-rectangle.drawRect(0, 0, 20, 20);
-rectangle.endFill();
-rectangle.x = screen.width/2;
-rectangle.y = screen.height/2;
-
 PIXI.loader
-  .add("../resources/villager_male.png")
+  .add("../resources/townhouse.png")
   .load(setup);
 
 function setup() {
-  sprite = new PIXI.Sprite(
-    PIXI.loader.resources["../resources/villager_male.png"].texture
+  townhouse = new PIXI.Sprite(
+    PIXI.loader.resources["../resources/townhouse.png"].texture
   );
 
   state = update;
 
+  townhouse.x = screen.width/2-60;
+  townhouse.y = screen.height/2-50;
 
-  game.addChild(rectangle);
-  game.addChild(sprite);
+
+  game.addChild(townhouse);
   renderer.render(game);
 }
 
@@ -40,7 +35,7 @@ function gameLoop() {
   renderer.render(game);
 }
 function update(){
-  sprite.x +=1;
+
 }
 
 gameLoop();
