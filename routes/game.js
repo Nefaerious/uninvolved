e7c45dcebb3698321901fac4a6ec4c001b5090ad
nefaerious <samshin102@gmail.com>
2017-11-11 20:36:11 -0600

    testing

--- routes/game.js
+++ routes/game.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('game/game');
+  res.render('game/game.js');
 });
 
 module.exports = router;
