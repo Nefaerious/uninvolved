abe62028594021f5f2147893294f419a2c835c27
nefaerious <samshin102@gmail.com>
2017-11-11 22:26:24 -0600

    fixes

--- resources/game/canvas.js
+++ resources/game/canvas.js
@@ -1,6 +1,7 @@
+var ctx;
 window.onload = function(){
   var canvas = document.getElementById("paper");
-  var ctx = canvas.getContext("2d");
+  ctx = canvas.getContext("2d");
   ctx.fillStyle = "silver";
   ctx.fillRect(50,50,50,50);
 };
